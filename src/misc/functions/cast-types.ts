507404f3942fc5b9ba6b5a5c1142729b6efa1ce6
carrichi <roberto@carrichi.com>
2024-06-19 01:06:30 -0600

    fix: :bug: Fix castDates function if item is null

--- src/misc/functions/cast-types.ts
+++ src/misc/functions/cast-types.ts
@@ -1,10 +1,16 @@
 export function castDates(field: string, items: any[]): any[] {
   const casted_items = [];
+  if (items.length == 0) return [];
+
   for (const item of items) {
     Object.keys(item).map((key) => {
+      // If field is null, pass to next record.
+      if (key === field && item[field] == null) casted_items.push(item);
+
+      // If field is a string, cast field and go next.
       if (
-        typeof item[key] === 'string' &&
         key === field &&
+        typeof item[field] === 'string' &&
         (item[field] = new Date(item[field]))
       )
         casted_items.push(item);
